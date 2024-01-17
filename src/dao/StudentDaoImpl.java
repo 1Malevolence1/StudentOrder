92a771af26ec159a67e0b4dca2b51d948b741222
Mr.D <danil36rusdanil@mail.ru>
2024-01-17 18:14:50 +0300

    изменил код в методе getStudentOrder

--- src/dao/StudentDaoImpl.java
+++ src/dao/StudentDaoImpl.java
@@ -5,11 +5,9 @@ import domain.*;
 import exception.DaoException;
 
 import java.sql.*;
+import java.sql.Date;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class StudentDaoImpl implements StudentOrderDao {
@@ -55,6 +53,22 @@ public class StudentDaoImpl implements StudentOrderDao {
                     "FROM jc_student_child soc " +
                     "INNER JOIN jc_register_office ro ON ro.r_office_id = soc.c_register_office_id " +
                     "WHERE soc.student_order_id IN ";
+
+
+    private static final String SELECT_ORDER_FULL =
+            "SELECT so.*, ro.r_office_area_id, ro.r_office_name, " +
+                    "po_h.p_office_area_id as h_p_office_area_id, " +
+                    "po_h.p_office_name as h_p_office_name, " +
+                    "po_w.p_office_area_id as w_p_office_area_id, " +
+                    "po_w.p_office_name as w_p_office_name, " +
+                    "soc.*, ro_c.r_office_area_id, ro_c.r_office_name " +
+                    "FROM jc_student_order so " +
+                    "INNER JOIN jc_register_office ro ON ro.r_office_id = so.register_office_id " +
+                    "INNER JOIN jc_passport_office po_h ON po_h.p_office_id = so.h_passport_office_id " +
+                    "INNER JOIN jc_passport_office po_w ON po_w.p_office_id = so.w_passport_office_id " +
+                    "INNER JOIN jc_student_child soc ON soc.student_order_id = so.student_order_id " +
+                    "INNER JOIN jc_register_office ro_c ON ro_c.r_office_id = soc.c_register_office_id " +
+                    "WHERE student_order_status = ? ORDER BY so.student_order_id";
 
     private Connection getConnection() throws SQLException {
         return DriverManager.getConnection(
@@ -109,29 +123,35 @@ public class StudentDaoImpl implements StudentOrderDao {
     @Override
     public List<StudentOrder> getStudentOrder() throws SQLException {
         List<StudentOrder> listStudentOrder = new LinkedList<>();
+
+
         try (Connection connection = getConnection();
-             PreparedStatement stmt = getStmt(connection, SELECT_ORDER)) {
+             PreparedStatement stmt = getStmt(connection, SELECT_ORDER_FULL)) {
+
+            Map<Long, StudentOrder> maps = new HashMap<>();
 
             stmt.setInt(1, StudentOrderStatus.START.ordinal());
-            ResultSet rs = stmt.executeQuery();
+            ResultSet rs= stmt.executeQuery();
 
-            List<Long> ids = new LinkedList<>();
             while (rs.next()) {
-                StudentOrder studentOrder = new StudentOrder();
-                fillStudentOrder(rs, studentOrder);
-                fillWedding(rs, studentOrder);
+                Long id = rs.getLong("student_order_id");
+                if (!maps.containsKey(id)) {
+                    StudentOrder studentOrder = new StudentOrder();
 
-                Adult husband = fillingAdult(rs, "h_");
-                Adult wife = fillingAdult(rs, "w_");
-                studentOrder.setHusband(husband);
-                studentOrder.setWife(wife);
+                    fillStudentOrder(rs, studentOrder);
+                    fillWedding(rs, studentOrder);
 
-                ids.add(studentOrder.getStudentOrderID());
-                listStudentOrder.add(studentOrder);
+                    Adult husband = fillingAdult(rs, "h_");
+                    Adult wife = fillingAdult(rs, "w_");
+                    studentOrder.setHusband(husband);
+                    studentOrder.setWife(wife);
+
+                    listStudentOrder.add(studentOrder);
+                    maps.put(id,studentOrder);
+                }
+                StudentOrder studentOrder = maps.get(id);
+                studentOrder.addChild(fillChild(rs));
             }
-
-            findChildren(connection, listStudentOrder);
-
 
         } catch (SQLException ex) {
             throw new SQLException();
